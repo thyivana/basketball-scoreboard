e46c0c0555d6aa2a3cb9c0a569d2dc938f74af04
thyivana <christabel1903@gmail.com>
2025-02-07 03:41:35 +0000

    basketball screboard
    second solo project

---
+++ scoreboard/index.js
@@ -0,0 +1,35 @@
+let sumEl = document.getElementById("sum-el")
+let countEl = document.getElementById("count-el")
+let count = 12
+
+function add1() {
+    count += 1
+    countEl.textContent = count
+}
+
+function add2() {
+    count += 2
+    countEl.textContent = count
+}
+
+function add3() {
+    count += 3
+    countEl.textContent = count
+}
+
+let sum = 5
+
+function snd1() {
+    sum += 1
+    sumEl.textContent = sum
+}
+
+function snd2() {
+    sum += 2
+    sumEl.textContent = sum                                         
+}
+
+function snd3() {
+    sum += 3
+    sumEl.textContent = sum
+}
